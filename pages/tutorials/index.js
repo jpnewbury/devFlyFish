58b6ca3d97167ea44ed8988b25fdd9041cd4e711
floydiology <john.newbury@gmail.com>
2021-02-13 09:52:21 -0700

    bug fix

--- pages/tutorials/index.js
+++ pages/tutorials/index.js
@@ -5,16 +5,13 @@ import Bugbar from "../../components/bugbar";
 export default function index() {
   return (
     <div className="container">
-      <Layout>
-        <main className="content">
-          <Bugbar />
-          <section className="section">
-            <span className="first-letter">
-              <p>Future Home of the Tutorials</p>
-            </span>
-          </section>
-        </main>
-      </Layout>
+      <main className="content">
+        <section className="section">
+          <span className="first-letter">
+            <p>Future Home of the Tutorials</p>
+          </span>
+        </section>
+      </main>
     </div>
   );
 }
