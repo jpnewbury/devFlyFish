66ef5e57ba4a4fdad505f955f4462e98420bef8f
floydiology <john.newbury@gmail.com>
2021-02-15 07:42:53 -0700

    new stuff

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,19 @@
-import React from "react";
+import Image from "next/image";
 
 export default function footer() {
   return (
-    <div className="center">
-      <p>2021 John P. Newbury Art & Design</p>
+    <div className="col-2l">
+      <div className="block">
+        <Image
+          src="/crLogo.png"
+          width={200}
+          height={125}
+          alt="Catch and Release"
+        />
+      </div>
+      <div className="center">
+        <p>2021 John P. Newbury Art & Design</p>
+      </div>
     </div>
   );
 }
