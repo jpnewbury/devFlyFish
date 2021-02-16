37fb0efde34180aff57b29443f3317bc7abb035e
floydiology <john.newbury@gmail.com>
2021-02-16 14:59:50 -0700

    fix

--- pages/articles/conditions.js
+++ pages/articles/conditions.js
@@ -26,7 +26,7 @@ export default function Home() {
       <div className="container">
         <main className="content">
           <header className="header">
-            <Header title="A Space for Fly Fishers" background="anothony" />
+            <Header title="A Space for Fly Fishers" background="anthony" />
           </header>
           <h2>Be Prepared for varirying conditions</h2>
           <section className="section1">
