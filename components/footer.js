83f0fb24dd5121f9e7de4bf42c23293e1762ec51
floydiology <john.newbury@gmail.com>
2021-02-26 08:48:17 -0700

    footer

--- components/footer.js
+++ components/footer.js
@@ -3,19 +3,14 @@ import Link from "next/link";
 
 export default function footer() {
   return (
-    <div className="col-2l">
-      <div className="block">
-        <Image
-          src="/crLogo.png"
-          width={200}
-          height={125}
-          alt="Catch and Release"
-        />
+    <div className="center">
+      <div>
+        <Image src="/cr.png" width={200} height={125} alt="Catch and Release" />
       </div>
       <div className="center">
         <Link href="https://portfolio-theta-orpin.vercel.app/">
           <a>
-            <p>2021 John P. Newbury Art & Design</p>
+            <p>© 2021 all content on this site John P. Newbury Art & Design</p>
           </a>
         </Link>
       </div>
