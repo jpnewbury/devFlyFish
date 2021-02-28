efead5cde76d543045f00e44a2f57ee4a4d75240
floydiology <john.newbury@gmail.com>
2021-02-28 09:41:09 -0700

    page transitions

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,13 @@ import { motion, AnimatePresence } from "framer-motion";
 function MyApp({ Component, pageProps, router }) {
   return (
     <AnimatePresence>
-      <Component {...pageProps} />
+      <motion.div
+        animate={{
+          opacity: [0.8, 1],
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </AnimatePresence>
   );
 }
