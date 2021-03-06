3e6d4164e7af55ccfac9b4647d557a59b66cd8bb
floydiology <john.newbury@gmail.com>
2021-03-06 08:50:57 -0700

    link edit

--- pages/tutorials/index.js
+++ pages/tutorials/index.js
@@ -38,7 +38,7 @@ export default function Top({ movies: patterns }) {
         <section className="section">
           <h2>Fly Tying Tutorials</h2>
           <div className="grid-ish">
-            <Link href="/tutorials/softie">
+            <Link href="/tutorials/shilea">
               <div className="card">
                 <h2>Tying a Femme Bot</h2>
 
