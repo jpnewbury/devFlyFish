9462b10b1b09f620b313c7a9c5eb662eb08b0b79
floydiology <john.newbury@gmail.com>
2021-03-28 07:23:17 -0600

    responsive bugbar

--- components/bugbarNav.js
+++ components/bugbarNav.js
@@ -6,55 +6,78 @@ import Hydropsyche from "./svg/hydropsyche";
 import Caddis from "./svg/caddis";
 import Midges from "./svg/midge";
 import Left from "./svg/bugbar";
+
 export default function bugbarNav() {
   return (
-    <div className="">
-      <div className="bugbarNav__col">
-        <div>
+    <div className="section2">
+      <div className="col-2l">
+        <div className="padding-left">
           <Left />
         </div>
         <div>
-          <Clinger style="icon" />
-          <span className="center">
-            <small>clingers</small>
-          </span>
-        </div>
-        <div>
-          <Crawler style="icon" />
-          <span className="center">
-            <small>crawlers</small>
-          </span>
-        </div>
-        <div>
-          <Swimmer style="icon" />
-          <span className="center">
-            <small>minnow</small>
-          </span>
-        </div>
-        <div>
-          <Stones style="icon" />
-          <span className="center">
-            <small>stoneflies</small>
-          </span>
-        </div>
-        <div>
-          <Hydropsyche style="icon" />
-          <span className="center">
-            <small>larva</small>
-          </span>
-        </div>
+          <div className="grid-small">
+            <div className="bug-card">
+              <div>
+                <Clinger style="icon-1" />
+              </div>
+              <div>
+                <small>clingers</small>
+              </div>
+            </div>
 
-        <div>
-          <Caddis style="icon" />
-          <span className="center">
-            <small>pupa</small>
-          </span>
-        </div>
-        <div>
-          <Midges style="icon" />
-          <span className="center">
-            <small>midges</small>
-          </span>
+            <div className="bug-card">
+              <div>
+                <Crawler style="icon-1" />
+              </div>
+              <div>
+                <small>crawlers</small>
+              </div>
+            </div>
+
+            <div className="bug-card">
+              <div>
+                <Swimmer style="icon-1" />
+              </div>
+              <div>
+                <small>minnow</small>
+              </div>
+            </div>
+            <div className="bug-card">
+              <div>
+                <Stones style="icon-1" />
+              </div>
+              <div>
+                <small>stoneflies</small>
+              </div>
+            </div>
+
+            <div className="bug-card">
+              <div>
+                <Hydropsyche style="icon-1" />
+              </div>
+              <div>
+                <small>larva</small>
+              </div>
+            </div>
+
+            <div className="bug-card">
+              <div>
+                <Caddis style="icon-1" />
+              </div>
+              <div>
+                <small>pupa</small>
+              </div>
+            </div>
+
+            <div className="bug-card">
+              <div>
+                <Midges style="icon-1" />
+              </div>
+              <div>
+                <small>midges</small>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
