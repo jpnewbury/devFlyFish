94df274ad93bc109270d76e2e7af6fa54dcd53ab
floydiology <john.newbury@gmail.com>
2021-04-22 09:24:02 -0600

    re-deploy

--- pages/sandbox.js
+++ pages/sandbox.js
@@ -1,39 +1,22 @@
+import useSwr from "swr";
 import Link from "next/link";
-import Image from "next/image";
 
-import Head from "next/head";
-import Header from "../components/header";
-import Footer from "../components/footer";
-import Leadfree from "../components/svg/leadFree";
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export default function index(props) {
+export default function Index() {
+  const { data, error } = useSwr(
+    "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=09085000&parameterCd=00060,00065&siteType=ST",
+    fetcher
+  );
+
+  if (error) return <div>Failed to load users</div>;
+  if (!data) return <div>Loading...</div>;
+
   return (
-    <>
-      <Head>
-        <title>John P. Newbury's Angling Arts</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta
-          name="keywords"
-          content="Fly Fishing, Fly Tying, Newbury Fly Tying, Fullingmill"
-        />
-        <meta
-          name="description"
-          content="A place for fly fishers to to learn about some new fly patterns and fishing methods from a life-long fly fishing addict."
-        />
-        <meta name="revised" content="Angling Arts, 2/14/2021" />
-      </Head>
-      <div className="container">
-        <main className="content">
-          <header className="header">
-            <Header title="Reading" background="anthony" />
-          </header>
-          <h2>Reading List</h2>
-          <section className="section">
-            <Leadfree />
-          </section>
-        </main>
-        <Footer />
-      </div>
-    </>
+    <ul>
+      {data.map((user) => (
+        <li key={user.id}></li>
+      ))}
+    </ul>
   );
 }
