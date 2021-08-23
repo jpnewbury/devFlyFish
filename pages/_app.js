0596c64f268e5300465ae5e5781ac26c7cb60f2f
floydiology <john.newbury@gmail.com>
2021-08-23 12:58:10 -0700

    added loading screen overlay

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,44 @@
 import "../styles/main.css";
-
+import Loading from "../components/loading";
+import { useState } from "react";
+import Router from "next/router";
 import { motion, AnimatePresence } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }) {
+  // loading screen when route changes
+  const [loading, setLoading] = useState(false);
+  Router.events.on("routeChangeStart", (url) => {
+    console.log(url);
+    setLoading(true);
+  });
+  Router.events.on("routeChangeComplete", () => {
+    setLoading(false);
+    console.log("Loading stop");
+  });
+
   return (
-    <AnimatePresence>
-      <motion.div
-      // animate={{
-      //   opacity: [0.8, 1],
-      // }}
-      >
-        <Component {...pageProps} />
-      </motion.div>
-    </AnimatePresence>
+    <>
+      {loading && (
+        <div className="modal">
+          <Loading style="loader" />
+        </div>
+      )}
+      <AnimatePresence exitBeforeEnter>
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              duration: 0.1,
+            },
+          }}
+          exit={{ opacity: 0 }}
+        >
+          <Component {...pageProps} key={router.route} />
+        </motion.div>
+      </AnimatePresence>
+    </>
   );
 }
 
